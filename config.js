63d73ca6e6b4da2aaf9c8e7fe3f3acd5c12c6343
emanuelefalli <92868195+emanuelefalli@users.noreply.github.com>
2026-07-16 12:16:48 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // URL che ottieni dopo aver pubblicato lo script come Web App (vedi README)
 const CONFIG = {
-  scriptUrl: "https://script.google.com/macros/s/AKfycbzteHyCOym7zLWKy6eBclnoHiL_0olSqai93q6x9egJZ5D0JeENRB-7Z2cDxa7pAJY/exec",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbwl09P8icAKgJgxM8AdnmC0c6baNcbYj5I4O7I0qYUwVFRhIzsca1y-m3wllqb5nQ8/exec",
   secretKey: "EmanueleFalli99*xyz@" // deve essere identica a SECRET_KEY in AppsScript.gs
 };
